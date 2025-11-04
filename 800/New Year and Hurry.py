f7dd5042263aa19d0814dd87071a8dfe7d41291b
MD. Rashedul Islam Sabbir <rashedulislamsabbir@gmail.com>
2025-11-04 21:50:59 +0600

    Update New Year and Hurry.py

--- 800/New Year and Hurry.py
+++ 800/New Year and Hurry.py
@@ -1,5 +1,7 @@
 # https://codeforces.com/problemset/problem/750/A
-import sys
+import io,os,sys
+input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+print = sys.stdout.write
 c = 0
 n,k = map(int,input().split())
 time = 240
@@ -7,4 +9,4 @@ for i in range(1,n+1):
     time -= 5*i
     if time >= k:
         c += 1
-sys.stdout.write(str(c))
+print(str(c))
